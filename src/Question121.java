1ae0da3530534b0ce27fafe6c73bdb1d997376b0
zhengshaoqi <1050872171@qq.com>
2023-06-14 08:56:34 +0800

    Add a second solution to question 121

--- src/Question121.java
+++ src/Question121.java
@@ -1,4 +1,6 @@
 public class Question121 {
+    //Leetcode实测第一种解法效率优于动态规划。尽量用第一种。
+
     /*
     如果我们真的在买卖股票，我们肯定会想：如果我是在历史最低点买的股票就好了！
     太好了，在题目中，我们只要用一个变量记录一个历史最低价格 minprice，我们就可以假设自己的股票是在那天买的。
@@ -17,5 +19,23 @@ public class Question121 {
         return maxProfit;
     }
 
-    //todo 后续补充动态规划解法，感觉会更通用。
+    /*
+    动态规划解法。注意状态转移方程是dp[i] = Math.max(dp[i - 1], prices[i] - minPrice)，因为只能买卖一次。
+    时间复杂度O(n);空间复杂度由于使用了滚动数组代替dp数组，所以为O(1)。
+     */
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int length = prices.length;
+        int p = 0;
+        int r = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            r = Math.max(p, prices[i] - minPrice);
+            p = r;
+        }
+        return r;
+    }
 }
