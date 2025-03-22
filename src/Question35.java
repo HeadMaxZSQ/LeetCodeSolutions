f1fa4ac6da5486a80c83d191501be6949d0f840d
zhengshaoqi <1050872171@qq.com>
2025-03-22 23:47:21 +0800

    Optimized the algorithm I wrote before.

--- src/Question35.java
+++ src/Question35.java
@@ -3,34 +3,33 @@
  */
 public class Question35 {
     /**
-     * 【最优解】【自己想的】普通二分查找查找目标值，没找到的话最后再根据情况进行处理
+     * 【真正的最优解】【头Max二分方法论】
      * 时间复杂度：二分查找的时间复杂度为O(logN)
      * 空间复杂度：O(1)
      */
-    public int searchInsert(int[] nums, int target) {
+    public int searchInsert2(int[] nums, int target) {
+        //按照2025.3.22借助DeepSeek得出的方法论来解决
+        //这里可以把题目理解为：寻找所有小于target的数字的右边界的下标，即小于target的最大数字的下标。若过程中找到target则直接返回。
+        int smallerIndex = -1;//【注意】边界问题增加一个变量用于存储最终结果下标，这个下标可能不存在，所以初始值为-1；【找右边界把结果初始值设为-1】
         int left = 0;
         int right = nums.length - 1;
-
-        //注意，left < right这种写法退出条件为left==right，此时不会进入循环进行处理，所以循环后面要再判断一下nums[left] == target是否成立。
-        while (left < right) {
-            //【注意】这里不要写为mid = (left + right) / 2，以防int越界。
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
+                smallerIndex = mid;
                 left = mid + 1;
             } else if (nums[mid] > target) {
                 right = mid - 1;
             }
         }
 
-        //因为循环退出条件是left==right，所以这里用left和right都一样
-        if (nums[left] == target) {
-            return left;
-        } else if (nums[left] < target) {//要插入的位置在left左边
-            return left + 1;
-        } else {//target比当前位置值小，但是肯定比left-1的值大，所以要插入的位置在当前位置
-            return left;
+        if (smallerIndex == -1) {
+            //说明不存在比target小的元素
+            return 0;
+        }  else {
+            return smallerIndex + 1;
         }
     }
 }
