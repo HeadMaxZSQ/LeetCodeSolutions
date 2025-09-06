eb170405bd74da279cc0119063891c36d0ea6ab0
zhengshaoqi <1050872171@qq.com>
2025-09-06 10:19:16 +0800

    add comments

--- src/Question70.java
+++ src/Question70.java
@@ -4,6 +4,8 @@
 public class Question70 {
     /**
      * 【自己想的】【运行超时!!!】【暴力递归】暴力递归进行动态规划，因为会重复计算某些数字，导致运行超时。
+     * 时间复杂度：O(2^n)，深度为n的二叉树递归遍历
+     * 空间复杂度：O(n)，深度为n的二叉树递归遍历
      */
     public int climbStairs(int n) {
         if (n <= 2) return n;
@@ -14,6 +16,8 @@ public class Question70 {
     /**
      * 【题解看的】【较优解】使用滚动数组的动态规划。
      * tips：暴力递归是反向的从n往前倒推，滚动数组数是正向地从1往后计算。
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
      */
     public int climbStairs1(int n) {
         //f(n) = f(n - 1) + f(n - 2)，
