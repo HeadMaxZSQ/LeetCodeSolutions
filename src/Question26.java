a8b65a51a3820fe0b17ab532291451cefdd93111
xiaoxin <1050872171@qq.com>
2026-03-08 21:03:26 +0800

    Add question 26 solutions

--- src/Question26.java
+++ src/Question26.java
@@ -1,3 +1,6 @@
+/**
+ * 26. 删除有序数组中的重复项
+ */
 public class Question26 {
     //快慢指针
     public int removeDuplicates(int[] nums) {
@@ -8,6 +11,21 @@ public class Question26 {
             if (nums[fast] != nums[fast - 1]) {
                 nums[slow] = nums[fast];
                 slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    //快慢指针，更容易理解的写法
+    public int removeDuplicates2(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        int currentValue = Integer.MIN_VALUE;
+        while (fast < nums.length) {
+            if (nums[fast] != currentValue) {
+                nums[slow++] = nums[fast];
+                currentValue = nums[fast];
             }
             fast++;
         }
